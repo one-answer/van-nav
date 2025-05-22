9dacaf2c88f9862ab50d298185efb1f4d67cbe0d
Oliver <wqdyxnbd@163.com>
2025-05-22 16:56:11 +0800

    rm github addr print

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -6,7 +6,6 @@ import * as serviceWorker from "./serviceWorker";
 // import reportWebVitals from './reportWebVitals';
 
 console.log("欢迎使用 狗窝导航 项目")
-console.log("项目地址: https://github.com/mereithhh/van-nav")
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
